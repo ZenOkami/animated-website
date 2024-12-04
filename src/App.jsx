b78cb682aae829d48ef415f5f5cd908677157242
ZenOkami <gianstasi@yahoo.com>
2024-12-03 20:47:38 -0700

    Animated Website First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <main>
+        <h1 className="text-5xl text-orange-500 font-bold">Amazing Animations</h1>
+      </main>
+    </>
+  )
+}
+
+export default App
