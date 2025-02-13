772ce62a368cca76641d3e953b02aaa7e1335e0e
unknown <gianstasi@yahoo.com>
2025-02-12 18:38:33 -0700

    Completed Story section and hovering image

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import Hero from './components/Hero'
 import { About } from './components/About'
 import { NavBar } from './components/NavBar'
 import { Features } from './components/Features'
+import { Story } from './components/Story'
+import { Contact } from './components/Contact'
+import { Footer } from './components/Footer'
 
 function App() {
 
@@ -14,8 +17,10 @@ function App() {
         <Hero />
         <About />
         <Features />
+        <Story />
+        <Contact />
+        <Footer />
 
-        <section className='z-0 min-h-screen bg-blue-500' />
       </main>
     </>
   )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer = () => {
+    return ( 
+        <footer className="footer">
+            <div className="container">
+                <p>�� 2023 Animated Website. All rights reserved.</p>
+            </div>
+        </footer>
+     );
+}
